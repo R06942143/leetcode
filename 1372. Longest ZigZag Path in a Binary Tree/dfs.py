e488e8d0315fc89c7d5e78b70146eebb8cfba691
R06942143 <r06942143@ntu.edu.tw>
2023-04-19 10:31:12 +0800

    chore: use blake to re-format

--- 1372. Longest ZigZag Path in a Binary Tree/dfs.py
+++ 1372. Longest ZigZag Path in a Binary Tree/dfs.py
@@ -1,7 +1,13 @@
 class Solution:
     def longestZigZag(self, root):
         def dfs(root):
-            if not root: return [-1, -1, -1]
+            if not root:
+                return [-1, -1, -1]
             left, right = dfs(root.left), dfs(root.right)
-            return [left[1] + 1, right[0] + 1, max(left[1] + 1, right[0] + 1, left[2], right[2])]
+            return [
+                left[1] + 1,
+                right[0] + 1,
+                max(left[1] + 1, right[0] + 1, left[2], right[2]),
+            ]
+
         return dfs(root)[-1]
