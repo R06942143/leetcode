f2f5197805f0ed7138ead9031dbb911f0a8e9632
conradSHL <r06942143@ntu.edu.tw>
2023-05-04 20:39:12 +0800

    feat: 413 ans

---
+++ 413. Arithmetic Slices/solution.py
@@ -0,0 +1,7 @@
+class Solution:
+    def numberOfArithmeticSlices(self, nums: list[int]) -> int:
+        dp = [0] * len(nums)
+        for i in range(2, len(nums)):
+            if nums[i - 1] - nums[i - 2] == nums[i] - nums[i - 1]:
+                dp[i] = dp[i - 1] + 1
+        return sum(dp)
