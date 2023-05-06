2c53c92f2d2c2461c3843110cc6dfa7e0f2e871a
conradSHL <r06942143@ntu.edu.tw>
2023-05-06 15:16:39 +0800

    feat: 1498 ans faster version

--- 1498. Number of Subsequences That Satisfy the Given Sum Condition/solution.py
+++ 1498. Number of Subsequences That Satisfy the Given Sum Condition/solution.py
@@ -1,13 +1,12 @@
 class Solution:
     def numSubseq(self, nums: list[int], target: int) -> int:
-        mod = 10**9 + 7
         ans = 0
-        r, l = len(nums) - 1, 0
         nums.sort()
+        l, r = 0, len(nums) - 1
         while l <= r:
-            if nums[l] + nums[r] > target:
-                r -= 1
-            else:
-                ans += 2 ** (r - l)
+            if nums[l] + nums[r] <= target:
+                ans += pow(2, (r - l), 1000000007)
                 l += 1
-        return ans % mod
+            else:
+                r -= 1
+        return ans % 1000000007
