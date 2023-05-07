98b50448e56ac2cc06cb56a0d9a8f0bd2490e549
conradSHL <r06942143@ntu.edu.tw>
2023-05-07 13:07:30 +0800

    feat: 1964 need to review

---
+++ 1964. Find the Longest Valid Obstacle Course at Each Position/solution.py
@@ -0,0 +1,17 @@
+from bisect import bisect_right
+
+
+class Solution:
+    def longestObstacleCourseAtEachPosition(self, nums: list[int]) -> List[int]:
+        lis = []
+        for i, x in enumerate(nums):
+            if (
+                len(lis) == 0 or lis[-1] <= x
+            ):  # Append to LIS if new element is >= last element in LIS
+                lis.append(x)
+                nums[i] = len(lis)
+            else:
+                idx = bisect_right(lis, x)  # Find the index of the smallest number > x
+                lis[idx] = x  # Replace that number with x
+                nums[i] = idx + 1
+        return nums
