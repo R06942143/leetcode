342738652a67bf60f5de6b57d398e742bee93554
conradSHL <r06942143@ntu.edu.tw>
2023-05-21 11:50:10 +0800

    feat: add a smart answer

--- 274. H-Index/solution.go
+++ 274. H-Index/solution.go
@@ -5,6 +5,19 @@ import (
 	"sort"
 )
 
+func otherPeopleSmartVersionHIndex(citations []int) int {
+	m, max := make(map[int]int), 0
+	for _, v := range citations {
+		// counter map
+		m[v]++
+	}
+	for i, h := 0, len(citations); i <= h; i++ {
+		max = i
+		h = h - m[i]
+	}
+	return max
+}
+
 func hIndex(citations []int) int {
 	sort.Ints(citations)
 	hIndex := 0
