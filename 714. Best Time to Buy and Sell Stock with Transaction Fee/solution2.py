98c416f755b203e84f3af0ef13338ac370968e3a
conradSHL <r06942143@ntu.edu.tw>
2023-06-22 20:48:41 +0800

    feat: 714 ans

--- 714. Best Time to Buy and Sell Stock with Transaction Fee/solution2.py
+++ 714. Best Time to Buy and Sell Stock with Transaction Fee/solution2.py
@@ -1,5 +1,5 @@
 class Solution:
-    def maxProfit(self, prices: List[int], fee: int) -> int:
+    def maxProfit(self, prices: list[int], fee: int) -> int:
         min_p = prices[0] + fee
         res = 0
 
