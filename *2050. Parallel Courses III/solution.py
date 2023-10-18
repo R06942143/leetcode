388be2b054acad093519e4c68c711ba975372aeb
Conrad <r06942143@ntu.edu.tw>
2023-10-18 19:25:22 +0800

    feat: 2050 ans

---
+++ *2050. Parallel Courses III/solution.py
@@ -0,0 +1,26 @@
+from functools import cache
+from collections import defaultdict
+
+
+class Solution:
+    def minimumTime(self, n: int, relations: list[list[int]], time: list[int]) -> int:
+        @cache
+        def dfs(node):
+            if not graph[node]:
+                return time[node]
+            
+            ans = 0
+            for neighbor in graph[node]:
+                ans = max(ans, dfs(neighbor))
+
+            return time[node] + ans
+        
+        graph = defaultdict(list)
+        for (x, y) in relations:
+            graph[x - 1].append(y - 1)
+        
+        ans = 0
+        for node in range(n):
+            ans = max(ans, dfs(node))
+
+        return ans
