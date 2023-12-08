57d34a3a25920d7a8b97e62ce0930b1a04c7d698
Conrad <r06942143@ntu.edu.tw>
2023-12-08 08:54:06 +0800

    feat: 606 solution.py

---
+++ 606. Construct String from Binary Tree/solution.py
@@ -0,0 +1,33 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def tree2str(self, t: TreeNode) -> str:
+        sb = [] # init string builder
+        
+        # helper function to create result
+        def helper(node: TreeNode) -> None:
+            if not node:
+                return
+            
+            sb.append('(')                      # add 1st wrapping parenthesis
+            sb.append(str(node.val))
+            
+            helper(node.left)                   # process left recursively
+            
+            if not node.left and node.right:    # add parenthesis if left is empty but right exists
+                sb.append('()')
+                
+            helper(node.right)                  # process right recursively
+            
+            sb.append(')')                      # add 2nd wrapping parenthesis
+        
+        helper(t)
+
+        # trim 1st and last parenthesis build result string
+        return ''.join(sb[1:-1]) 
