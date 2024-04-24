c91c0e1baa8948ade5c1c4e5623ff76357945569
Conrad <r06942143@ntu.edu.tw>
2024-04-24 09:25:59 +0800

    feat: 1137 ans

---
+++ 1137. N-th Tribonacci Number/solution2.py
@@ -0,0 +1,13 @@
+from functools import lru_cache
+
+
+class Solution:
+    @lru_cache
+    def tribonacci(self, n: int) -> int:
+        if n == 0:
+            return 0
+        
+        if n == 1 or n == 2:
+            return 1
+        
+        return self.tribonacci(n-3) + self.tribonacci(n-2) + self.tribonacci(n-1)
