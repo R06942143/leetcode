7c6588af49e06565545fe38fa343349a3627af05
conrad <r06942143@ntu.edu.tw>
2024-11-14 19:43:33 +0800

    feat: 2064 golang solution

---
+++ 2064. Minimized Maximum of Products Distributed to Any Store/solution.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func minimizedMaximum(n int, quantities []int) int {
+    canDistribute := func(x int) bool {
+        stores := 0
+        for _, q := range quantities {
+            stores += (q + x -1) / x
+        }
+        return stores <= n
+    }
+    left, right := 1, 0
+    for _, q := range quantities {
+        if q > right {
+            right = q
+        }
+    }
+
+    result := 0
+    for left <= right{
+        x := (left + right) / 2
+        if canDistribute(x) {
+            result = x
+            right = x - 1
+        } else {
+            left = x + 1
+        }
+    }
+    return result
+}
+
+func main() {
+    fmt.Println(minimizedMaximum(6, []int{11, 6}))
+}
