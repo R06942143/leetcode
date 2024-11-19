3066d2e518a75adbe2d355a578d417d909eab5bf
conrad <r06942143@ntu.edu.tw>
2024-11-19 13:11:10 +0800

    feat: 2461 ans written in go

---
+++ 2461. Maximum Sum of Distinct Subarrays With Length K/solution.go
@@ -0,0 +1,37 @@
+package main
+
+func maximumSubarraySum(nums []int, k int) int64 {
+	n := len(nums)
+	elements := make(map[int]bool)
+	currentSum := int64(0)
+	maxSum := int64(0)
+	begin := 0
+
+	for end := 0; end < n; end++ {
+		if !elements[nums[end]] {
+			currentSum += int64(nums[end])
+			elements[nums[end]] = true
+
+			if end-begin+1 == k {
+				if currentSum > maxSum {
+					maxSum = currentSum
+				}
+				currentSum -= int64(nums[begin])
+				delete(elements, nums[begin])
+				begin++
+			}
+		} else {
+			for begin < end && nums[begin] != nums[end] {
+				currentSum -= int64(nums[begin])
+				delete(elements, nums[begin])
+				begin++
+			}
+			begin++
+		}
+	}
+	return maxSum
+}
+
+func main() {
+	println(maximumSubarraySum([]int{1, 5, 4, 2, 9, 9, 9}, 3))
+}
